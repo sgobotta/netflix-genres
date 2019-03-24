251b0c2872e4307e37ab4f55acd8d387126983b2
Santiago <sgobotta@gmail.com>
2019-03-24 15:40:17 -0300

    add logs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 const app = express();
+console.log('ENVIRONMENT', process.env)
 const port = process.env.PORT || 80;
 
 app.use(serveStatic(path.join(__dirname, 'dist')));
